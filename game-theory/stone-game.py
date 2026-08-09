40df79459a45a8b0dde99782fad871cc4b7879c2
Aadhya99A <aadhyareddy3000@gmail.com>
2026-08-09 16:00:18 +0530

    LC-stone-game [Runtime: N/A; Memory: N/A]

--- game-theory/stone-game.py
+++ game-theory/stone-game.py
@@ -1,22 +1,3 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        A, B = 0, 0
-        af = True
-        while len(piles) != 0:
-            if af:
-                if piles[0] > piles[-1]:
-                    A += piles.pop(0)
-                elif piles [0] < piles[-1]:
-                    A += piles.pop(-1)
-                else:
-                    A += piles.pop(-1)
-                af = False
-            else:
-                if piles[0] > piles[-1]:
-                    B += piles.pop(0)
-                elif piles [0] < piles[-1]:
-                    B += piles.pop(-1)
-                else:
-                    B += piles.pop(-1)
-                af = True
-        return A > B
+        return True
