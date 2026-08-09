6d5d8854d2741328a4f4cb557315d4383d7fc79c
Aadhya99A <aadhyareddy3000@gmail.com>
2026-08-09 17:27:35 +0530

    LC-score-of-a-string [Runtime: N/A; Memory: N/A]

---
+++ string/score-of-a-string.py
@@ -0,0 +1,6 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        ret = 0
+        for i in range(len(s) -1 ):
+            ret += abs(ord(s[i]) - ord(s[i+1]))
+        return ret
