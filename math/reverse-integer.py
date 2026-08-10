2aa6bd6a85315a832a8f8b52ae0c9cc24fc1c019
Aadhya99A <aadhyareddy3000@gmail.com>
2026-08-11 01:06:07 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

--- math/reverse-integer.py
+++ math/reverse-integer.py
@@ -1,8 +1,11 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x not in range(-2**31 + 1, 2**31 - 1):
-            return 0
+        k = None
         if x < 0:
-            return -int(str(abs(x))[::-1])
+            k = -int(str(abs(x))[::-1])
         else:
-            return int(str(x)[::-1])
+            k = int(str(x)[::-1])
+        if k not in range(-2**31 + 1, 2**31 - 1):
+            return 0
+        else:
+            return k
