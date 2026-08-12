1e551303aa74904cc0b5c7e0f9e9754ac6d43487
Aadhya99A <aadhyareddy3000@gmail.com>
2026-08-12 10:41:41 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- counting/majority-element.py
+++ counting/majority-element.py
@@ -1,15 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         ret = None
-        el = {x:0 for x in set(nums)}
-        for i in nums:
-            el[i] += 1
-        if len(nums)%2 == 0:
+        el = {x:nums.count(x) for x in set(nums)}
+        if len(nums) % 2 == 0:
             for key, value in el.items():
-                if value >= len(nums)/2:
+                if value >= len(nums) / 2:
                     ret = key
         else:
             for key, value in el.items():
-                if value >= len(nums)//2 + 1:
+                if value >= len(nums) // 2 + 1:
                     ret = key
         return ret
