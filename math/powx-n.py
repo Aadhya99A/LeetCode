e0e27f07b80ad3978325069592b7457e07e5234c
Aadhya99A <aadhyareddy3000@gmail.com>
2026-08-15 10:38:54 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

--- math/powx-n.py
+++ math/powx-n.py
@@ -1,7 +1,3 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-        #if x == 0:
-         #   return 0
-        return x*self.myPow(x, abs(n-1)) if n > 0 else x/(self.myPow(x, abs(n-1)))
+        return x**n
