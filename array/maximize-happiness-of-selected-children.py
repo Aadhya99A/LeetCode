aa3eb39985173f0fdc3440ecd2d558dd830a8630
Aadhya99A <aadhyareddy3000@gmail.com>
2026-08-25 22:32:44 +0530

    LC-maximize-happiness-of-selected-children [Runtime: N/A; Memory: N/A]

--- array/maximize-happiness-of-selected-children.py
+++ array/maximize-happiness-of-selected-children.py
@@ -1,11 +1,4 @@
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        valve = 1
-        counted = 0
-        for i in range(k):
-            if max(happiness) > 0:
-                valve += happiness.pop(happiness.index(max(happiness)))
-                counted += 1
-            else:
-                break
-        return valve - counted
+        happiness.sort(reverse=True)
+        return sum(max(0, h - i) for i, h in enumerate(happiness[:k]))
