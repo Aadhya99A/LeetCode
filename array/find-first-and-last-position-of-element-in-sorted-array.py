bb850b8f3db663da5fea6b60f2eef80ad05fea86
Aadhya99A <aadhyareddy3000@gmail.com>
2026-09-04 20:13:21 +0530

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: N/A; Memory: N/A]

--- array/find-first-and-last-position-of-element-in-sorted-array.py
+++ array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -6,7 +6,6 @@ class Solution:
         second = -1
         while lb <= rb:
             mid_1 = lb + (rb - lb)//2
-            print("Loop 1:", mid_1)
             if target == nums[mid_1]:
                 first = mid_1
                 rb = mid_1 - 1
@@ -14,12 +13,8 @@ class Solution:
                 lb = mid_1 + 1
             elif target < nums[mid_1]:
                 rb = mid_1 - 1
-        print(first)
         while l <= r:
             mid_2 = l + (r - l)//2
-            print("Loop 2:", mid_2)
-            if l == r:
-                break
             if target == nums[mid_2]:
                 second = mid_2
                 l = mid_2 + 1
@@ -27,5 +22,4 @@ class Solution:
                 l = mid_2 + 1
             elif target < nums[mid_2]:
                 r = mid_2 - 1
-        print(second)
         return [first, second]
