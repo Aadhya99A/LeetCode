7b76578acabdf4c9ef8edb3f77e8aa8943a019e6
Aadhya99A <aadhyareddy3000@gmail.com>
2026-09-05 22:39:23 +0530

    LC-smallest-stable-index-ii [Runtime: N/A; Memory: N/A]

--- array/smallest-stable-index-ii.py
+++ array/smallest-stable-index-ii.py
@@ -1,11 +1,19 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
-        stables = []
-        for i in range(len(nums)):
-            inst = max(nums[:i+1]) - min(nums[i:])
-            if inst <= k:
-                stables.append(i)
-        if stables:
-            return min(stables)
-        else:
+        n = len(nums)
+        if n == 0:
             return -1
+        pref_max = [0] * n
+        curr_max = nums[0]
+        for i in range(n):
+            curr_max = max(curr_max, nums[i])
+            pref_max[i] = curr_max
+        suff_min = [0] * n
+        curr_min = nums[-1]
+        for i in range(n - 1, -1, -1):
+            curr_min = min(curr_min, nums[i])
+            suff_min[i] = curr_min
+        for i in range(n):
+            if pref_max[i] - suff_min[i] <= k:
+                return i
+        return -1
