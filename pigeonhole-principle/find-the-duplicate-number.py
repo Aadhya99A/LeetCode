cd458c41a53f56b345d268cec2c1e907e89736e8
Aadhya99A <aadhyareddy3000@gmail.com>
2026-09-06 18:38:47 +0530

    LC-find-the-duplicate-number [Runtime: N/A; Memory: N/A]

--- pigeonhole-principle/find-the-duplicate-number.py
+++ pigeonhole-principle/find-the-duplicate-number.py
@@ -1,5 +1,11 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        for i in nums:
-            if nums.count(i) > 1:
-                return i
+        snm = sorted(nums)
+        old_curr = snm[0]
+        new_curr = snm[1]
+        for i in range(len(nums)):
+            if i > 0:
+                old_curr = snm[i-1]
+                new_curr = snm[i]
+            if new_curr == old_curr:
+                return old_curr
