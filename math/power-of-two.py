c206b2566bb177f3af8fb02d229542fc87fc8190
Aadhya99A <aadhyareddy3000@gmail.com>
2026-09-18 19:34:14 +0530

    LC-power-of-two [Runtime: N/A; Memory: N/A]

---
+++ math/power-of-two.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n < 0:
+            return False
+        for i in range(0, 32):
+            if 2**i == n:
+                return True
+        return False
