7068ce4eed19ef87ac6a202a503dd2e849aa79f6
Aadhya99A <aadhyareddy3000@gmail.com>
2026-09-18 19:47:29 +0530

    LC-power-of-three [Runtime: N/A; Memory: N/A]

--- recursion/power-of-three.py
+++ recursion/power-of-three.py
@@ -1,3 +1,9 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        return n in [1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, 14348907, 43046721, 129140163, 387420489, 1162261467]
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        if n%3 != 0:
+            return False
+        return self.isPowerOfThree(n//3)
